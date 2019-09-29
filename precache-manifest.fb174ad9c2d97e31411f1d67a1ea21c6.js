e36363d729c6b73f13cac6d9fbf22ad8b2b16269
mrcarjul <mrcarjul@gmail.com>
2019-09-29 00:42:47 -0500

    Updates

---
+++ precache-manifest.fb174ad9c2d97e31411f1d67a1ea21c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3e0076336304f57aeb9b7f561ae25f",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "bd3f9494d7b2245f54bf",
+    "url": "/landingpage/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "33c54b324671846edc0c",
+    "url": "/landingpage/static/js/2.799d88ae.chunk.js"
+  },
+  {
+    "revision": "bd3f9494d7b2245f54bf",
+    "url": "/landingpage/static/js/main.fffc22b7.chunk.js"
+  },
+  {
+    "revision": "8c0adfc9455b4c1678bf",
+    "url": "/landingpage/static/js/runtime-main.7c290522.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/landingpage/static/media/logo.25bf045c.svg"
+  }
+]);
