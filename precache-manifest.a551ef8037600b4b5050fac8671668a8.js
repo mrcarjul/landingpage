730571ed666eb1606e19951025c77f6be226a654
cortega3 <119902728+cortega3@users.noreply.github.com>
2025-07-30 08:59:35 -0600

    Updates

---
+++ precache-manifest.a551ef8037600b4b5050fac8671668a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b75af48293784e5daeb49a8991f7ae7",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "ec7e67604e582786b15a",
+    "url": "/landingpage/static/css/2.4dac051e.chunk.css"
+  },
+  {
+    "revision": "31637edfab31d5fba3e7",
+    "url": "/landingpage/static/css/main.68f40f52.chunk.css"
+  },
+  {
+    "revision": "ec7e67604e582786b15a",
+    "url": "/landingpage/static/js/2.9f945eb8.chunk.js"
+  },
+  {
+    "revision": "31637edfab31d5fba3e7",
+    "url": "/landingpage/static/js/main.71815bbb.chunk.js"
+  },
+  {
+    "revision": "6e75b5abbb1ec878ef2f",
+    "url": "/landingpage/static/js/runtime-main.034f408c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/landingpage/precache-manifest.b35bab9ddde4f00266a26941131710f4.js"
+  "/landingpage/precache-manifest.a551ef8037600b4b5050fac8671668a8.js"
 );
 
 self.addEventListener('message', (event) => {
